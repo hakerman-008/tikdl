aa91f7c8b430edda345e59dfac3073878d9d30e1
hakerman <142625808+hakerman-008@users.noreply.github.com>
2024-02-17 00:27:27 +0545

    Update index.js

--- index.js
+++ index.js
@@ -6,21 +6,8 @@ const app = express();
 app.get('/tiktok', async (req, res) => {
   const { url } = req.query;
 
-  const options = {
-    method: 'POST',
-    url: 'https://all-media-downloader1.p.rapidapi.com/tiktok',
-    headers: {
-      'content-type': 'application/json',
-      'X-RapidAPI-Key': 'b38444b5b7mshc6ce6bcd5c9e446p154fa1jsn7bbcfb025b3b',
-      'X-RapidAPI-Host': 'all-media-downloader1.p.rapidapi.com'
-    },
-    data: {
-      url: url
-    }
-  };
-
   try {
-    const response = await axios.request(options);
+    const response = await axios.get(`https://tools.betabotz.eu.org/tools/tiktokdl?url=${encodeURIComponent(url)}`);
     const videoUrl = response.data.result.data.play;
 
     res.json({ videoUrl });
@@ -29,7 +16,6 @@ app.get('/tiktok', async (req, res) => {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
